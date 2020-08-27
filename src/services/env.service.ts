e3fc1806a56e03c23ed9cac056260ed5941a8e11
hieupv <hieupv@codersvn.com>
2020-08-27 15:32:30 +0700

    try

--- src/services/env.service.ts
+++ src/services/env.service.ts
@@ -5,8 +5,9 @@ export class EnvService {
   async compile(file, values) {
     const content = fs.readFileSync(file, 'utf8');
     const compiled = template(content);
-    const data = compiled(values);
-    console.log(data);
-    return data;
+    const new_content = compiled(values);
+    console.log({ new_content });
+    fs.writeFileSync(file, new_content);
+    return new_content;
   }
 }
