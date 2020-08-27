48f71814f537abf9fe0d0ab3b5f1cf88c5ee084a
hieupv <hieupv@codersvn.com>
2020-08-27 15:26:12 +0700

    try

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-console.log(process.argv);
-console.log(process.env.PLUGIN_ENVS);
+import { EnvService } from './services/env.service';
+import * as path from 'path';
+import { get } from 'lodash';
+
+class App {
+  run() {
+    const envService = new EnvService();
+    const file = path.resolve(process.cwd(), '.env');
+    const values = {};
+    const envs = process.env.PLUGIN_ENVS.split(',');
+    envs.forEach((item) => {
+      values[item] = get(process.env, `PLUGIN_${item}`);
+    });
+    console.log({ values });
+    envService.compile(file, values);
+  }
+}
+
+const app = new App();
+app.run();
---
+++ src/services/env.service.ts
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import { template } from 'lodash';
+
+export class EnvService {
+  async compile(file, values) {
+    const content = fs.readFileSync(file, 'utf8');
+    const compiled = template(content);
+    const data = compiled(values);
+    console.log(data);
+    return data;
+  }
+}
